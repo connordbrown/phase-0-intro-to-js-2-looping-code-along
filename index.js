9c356650ef582b07c82d9d6cd6c79ac5cbde31ee
Connor <connordbrown@gmail.com>
2023-06-27 21:32:56 -0600

    completes functions and passes tests

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let arr = []
+    for(let i=0; i < names.length; i++) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return arr;
+}
+
+function countDown() {
+    for (let i=10; i >= 0; i--) {
+        console.log(i);
+    }
+}
